764892bc7b6c92a982b71735d4bf2a3fd4a92df7
albert papp <albert@papps.net>
2018-02-19 18:22:58 +0000

    Merged PR 44: Fixed scrolling layout in intro + added german translations
    Fixed scrolling layout in intro + added german translations
    
    Related work items: #154

--- blacksalt.abe/themes/hugo-elate-theme/static/js/main.js
+++ blacksalt.abe/themes/hugo-elate-theme/static/js/main.js
@@ -42,6 +42,24 @@
 
 	};
 
+	var goTo = function() {
+
+        $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+              if (target.length) {
+                $('html, body').animate({
+                  scrollTop: (target.offset().top - 54)
+                }, 500, "easeInOutExpo");
+                return false;
+              }
+            }
+          });
+        
+	
+	};
+
 
 	var goToTop = function() {
 
@@ -455,7 +473,7 @@
 		navigationSection();
 
 		goToTop();
-
+		goTo();
 
 		// Animations
 		homeAnimate();
